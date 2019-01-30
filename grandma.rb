1a917f2796d2418f92b35873d845e29a9f4a8c71
Joan in DC <joanrigdon@gmail.com>
2019-01-30 22:43:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma (phrase)
-  if phrase.downcase == phrase
+  if phrase === "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.downcase == phrase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase == phrase
     return "NO, NOT SINCE 1938!"
-  elsif phrase === "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
 
