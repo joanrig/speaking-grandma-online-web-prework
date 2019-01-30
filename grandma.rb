47bb01fdc731f8a32eafa0ce06c58d54fa053c46
Joan in DC <joanrigdon@gmail.com>
2019-01-30 22:41:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 def speak_to_grandma (phrase)
   if phrase.downcase == phrase
-    return ""
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase.upcase == phrase
+    return 
 
 
 # # Write a speak_to_grandma method.
